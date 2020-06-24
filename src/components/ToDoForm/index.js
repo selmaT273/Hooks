cd4df62786d44efe490776c3c90975d5dbf1be63
Keith Dahlby <keith@newbo.co>
2020-06-24 10:23:41 -0500

    Make sure we have a difficulty

--- src/components/ToDoForm/index.js
+++ src/components/ToDoForm/index.js
@@ -4,11 +4,18 @@ import useForm from '../../hooks/form.js';
 export default function ToDoForm(props){
 
     const { onSave } = props;
+    const requiredValues = {
+        difficulty: 5,
+        completed: false,
+    }
 
     let [handleSubmit, handleChange] = useForm(saveFromHook);
 
     function saveFromHook(formValues){
-        onSave({...formValues, completed: false});
+        onSave({
+            ...requiredValues,
+            ...formValues,
+        });
 
         // TODO: use fetch to POST
     }
@@ -25,7 +32,7 @@ export default function ToDoForm(props){
                 <input name="assignedTo" onChange={handleChange}></input>
 
                 <label>Difficulty</label>
-                <input name="difficulty" type="range" min="0" max="5" step="1" onChange={handleChange}></input>
+                <input name="difficulty" type="range" min="0" max="5" step="1" onChange={handleChange} />
                 <br></br>
                 <br></br>
                 <button type="submit">Save Todo Item</button>
